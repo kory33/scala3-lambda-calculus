2c4c36dcce3236903a18f1afe703b0d6d527f40c
kory33 <korygm33@gmail.com>
2024-10-31 02:34:21 +0900

    add initial implementation of CEK machines

---
+++ build.sbt
@@ -0,0 +1,12 @@
+scalaVersion := "3.5.0"
+libraryDependencies ++= Seq()
+
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-encoding",
+  "UTF-8",
+  "-feature",
+  "-unchecked",
+  "-Ypartial-unification",
+  "-Ykind-projector:underscores"
+)
---
+++ src/main/scala/Foundation.scala
@@ -0,0 +1,28 @@
+// 変数。
+// 実際のところ、Variable = AnyRef にしていい (値同士が区別可能で無限個あればなんでもいい) んですが、pretty-print しにくいのと、AnyRef をいっぱい作っておくのがひたすらに面倒なので、String を使います
+// ちなみに、Variable = BigInteger でももちろんいいです
+type Variable = String
+
+def freshVariableNotIn(variableSet: /* 有限 */ Set[Variable]): Variable = {
+  val alphabet = "abcdefghijklmnopqrstuvwxyz"
+  val alphabetSize = alphabet.length
+
+  def intToVariableName(n: Int): String = {
+    if (n <= alphabetSize) alphabet.charAt(n - 1).toString
+    else {
+      val res = n % alphabetSize
+      val div = n / alphabetSize
+      if (res == 0) intToVariableName(div - 1) + alphabet(alphabetSize - 1)
+      else intToVariableName(div) + alphabet(res - 1)
+    }
+  }
+
+  @annotation.tailrec
+  def loop(n: Int): Variable = {
+    val candidate = intToVariableName(n)
+    if variableSet(candidate) then loop(n + 1)
+    else candidate
+  }
+
+  loop(1)
+}
